b02fae41dc034a1ab33f02cedd0fb2c304074863
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-02-22 16:14:55 +0100

    fix: update addition example in App.java output

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven with CI!");
-        System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("5 + 3 = " + add(5, 3));
     }
 
     public static int add(int a, int b) {
